ce831a43cd8404d2dfe9e2542c69e5b293231de0
samuelsuffern <57135562+samuelsuffern@users.noreply.github.com>
2019-11-15 10:46:57 +0000

    webapps
    webapps

---
+++ webapps/WEB-INF/classes/Canal.java
@@ -0,0 +1,12 @@
+public class Canal{
+    private String nombreCanal;
+    private String grupo;
+    private Programa programa;
+   
+    public Canal(String nombreCanal, String grupo, Programa programa){
+        this.nombreCanal = nombreCanal;
+        this.grupo = grupo;
+        this.programa = programa:
+    }
+    
+}
