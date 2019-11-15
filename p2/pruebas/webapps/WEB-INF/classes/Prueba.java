7655b1aa0d333098900f20930f6eb07fb4b407d6
Samuel Suffern <pepito@pepito.com>
2019-11-15 10:03:48 +0100

    p2

---
+++ p2/pruebas/webapps/WEB-INF/classes/Prueba.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.net.URL;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.swing.text.html.parser.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.validation.Schema;
+import 	org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import javax.servlet.annotation.*;
+
+@WebServlet ("/Prueba")
+public class Prueba extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+    throws IOException, ServletException
+    {
+        res.setCharacterEncoding("utf-8");
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        String solicitante = req.getParameter("solicitante");
+        
+        out.println("<html>");
+        out.println("<head>");
+
+        out.println("<link rel='stylesheet' type='text/css' href='" + req.getContextPath() + "/css/prueba.css' >");
+        out.println("<meta charset='utf-8'/><title>¡Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("hola");
+        try{
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        
+            // Leo programacion, es padre -> elimino #text
+            // De programacion leo canales,es padre --> elimino #text
+            // De canal, leo programas, es padre -> elimino #text
+            
+            
+
+
+        Document doc = db.parse(new URL("http://gssi.det.uvigo.es/users/agil/public_html/SINT/19-20/tvml-2004-12-01.xml").openStream());
+        
+            NodeList canales = doc.getElementsByTagName("Canal");
+            Node padreCanal = canales.item(0).getParentNode();
+            out.println("Nodo padre " + padreCanal.getNodeName() + " Tiene " +padreCanal.getChildNodes().getLength() + " hijos"+  "<br>");
+            for(int i = 0 ; i <canales.getLength() ; i ++){
+                //Element elem = (Element) canales.item(i);
+                NamedNodeMap atributosC = canales.item(i).getAttributes();
+                
+                out.println("<br>");
+                out.println(canales.item(i).getNodeName() + " - "+ atributosC.item(1).getNodeName() + " " +atributosC.item(1).getNodeValue() );
+                out.println("<br>");
+                if(canales.item(i).hasChildNodes()){
+                    
+                    NodeList hijosCanal = canales.item(i).getChildNodes();
+                    out.println("hijos del canal: " + hijosCanal.getLength());
+                   // limpiar #text de un node que tiene hijos
+                    for(int j = 0 ; j < hijosCanal.getLength() ; j++){
+                        Node hijoC = hijosCanal.item(j);
+                        if(!(hijoC.getNodeName().equals("#text"))){
+                            
+                            out.println(hijosCanal.item(j).getNodeName() + " - ");
+                            
+                        }else{
+                            
+                            canales.item(i).removeChild(hijoC);
+                            j--;
+                        }
+
+
+                }
+
+                out.println("<br>"+hijosCanal.item(0).getChildNodes().item(0).getNodeValue());
+                       
+                }
+
+                    }
+
+            // Programa edadminima = "3" langs = "es"
+
+        	//System.out.println(node.item(0).getNodeValue());
+        	
+        }catch (Exception e) {
+            e.printStackTrace();
+            // TODO: handle exception
+        }
+        out.println("<h1>hola "+solicitante+"</h1>");
+        out.println("adios");
+
+        System.out.println("mensaje de log con System.out.println()");
+
+        this.log("mensaje de log con log()");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
